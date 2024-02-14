4fa784ef818f903bdde16ef10eb27ebceaf2f4ba
AKASH-PRASAD7 <akash.csel016@bppimt.ac.in>
2024-02-14 19:14:44 +0530

    feat: Auth service

---
+++ src/appwrite/auth.js
@@ -0,0 +1,59 @@
+import conf from "../conf/config.js";
+import { Client, Account, ID } from "appwrite";
+
+class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client.setEndpoint(conf.appwriteUrl).setEndpoint(conf.projecrId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const newUser = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (newUser) {
+        //signin user/call another method
+        return this.login(email, password);
+      } else {
+        return newUser;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async logout() {
+    try {
+      return await this.account.deleteSession();
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getCurrrntUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      throw error;
+    }
+    return null;
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
